9d470b82e2d8b449b623821dbd3a534456b94931
Kevin Gisi <kevin.gisi@calendly.com>
2023-03-29 10:23:42 -0500

    Bugfix in RenderableTransformer

--- src/hext-lang/ast/transformers/renderableTransformer.ts
+++ src/hext-lang/ast/transformers/renderableTransformer.ts
@@ -5,20 +5,26 @@ import { HexCoord, HexIcon, HexLabel, PathLabel } from "../nodes";
 export class RenderableTransformer extends Visitor {
 	visitHexDefinition(node: ASTNode) {
 		if (!node.children.renderables) node.children.renderables = [];
-		(node.children.renderables as ASTNode[]).push(
-			...[
-				new HexCoord({
-					primitives: { text: node.primitives.coordinates },
-				}),
-				new HexIcon({ primitives: { name: node.primitives.icon } }),
-				new HexLabel({ primitives: { text: node.primitives.label } }),
-			]
+		const renderables = node.children.renderables as ASTNode[];
+		renderables.push(
+			new HexCoord({ primitives: { text: node.primitives.coordinates } })
 		);
-		delete node.primitives.icon;
-		delete node.primitives.label;
+		if (node.primitives.icon) {
+			renderables.push(
+				new HexIcon({ primitives: { name: node.primitives.icon } })
+			);
+			delete node.primitives.icon;
+		}
+		if (node.primitives.label) {
+			renderables.push(
+				new HexLabel({ primitives: { name: node.primitives.label } })
+			);
+			delete node.primitives.label;
+		}
 	}
 
 	visitPathDefinition(node: ASTNode) {
+		if (!node.primitives.label) return;
 		if (!node.children.renderables) node.children.renderables = [];
 		(node.children.renderables as ASTNode[]).push(
 			new PathLabel({ primitives: { text: node.primitives.label } })
